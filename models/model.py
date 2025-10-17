e2791a09d4118cbc9e9fecbfd424e16fdd44ecfb
Jonah <rjonah321@gmail.com>
2025-10-17 08:02:04 -0400

    Removed code for old model

--- models/model.py
+++ models/model.py
@@ -38,52 +38,6 @@ class VideoAutoClipper(nn.Module):
         self.conv1dblock_3 = Conv1dBlock(128, 256, kernel_size=3, padding=4, dilation=4)
         self.conv1dblock_4 = Conv1dBlock(256, 512, kernel_size=3, padding=8, dilation=8)
 
-        self.conv1dblock_5 = Conv1dBlock(512, 1024, kernel_size=3, padding=16, dilation=16)
-        self.conv1dblock_6 = Conv1dBlock(1024, 2048, kernel_size=3, padding=32, dilation=32)
-
-        self.conv1dblock_7 = Conv1dBlock(2048, 4096, kernel_size=3, padding=16, dilation=16)
-        self.conv1dblock_8 = Conv1dBlock(4096, 1024, kernel_size=3, padding=8, dilation=8)
-
-        self.LSTM = nn.LSTM(1024, 1024, batch_first=True)
-        self.conv1dblock_9 = Conv1dBlock(1024, 256, kernel_size=3, padding=2, dilation=2)
-
-        self.dropout = nn.Dropout(0.5)
-        self.conv1dfinal = nn.Conv1d(256, 1, kernel_size=1)
-
-    def forward(self, X):
-        X = self.conv1dblock_1(X)
-        X = self.conv1dblock_2(X)
-
-        X = self.conv1dblock_3(X)
-        X = self.conv1dblock_4(X)
-
-        X = self.conv1dblock_5(X)
-        X = self.conv1dblock_6(X)
-
-        X = self.conv1dblock_7(X)
-        X = self.conv1dblock_8(X)
-
-        X = X.view(X.size(0), X.size(2), X.size(1))
-        X, _ = self.LSTM(X)
-        X = X.view(X.size(0), X.size(2), X.size(1))
-        X = self.conv1dblock_9(X)
-
-        X = self.dropout(X)
-        X = self.conv1dfinal(X)
-
-        return X.squeeze()
-
-
-class VideoAutoClipper2(nn.Module):
-    def __init__(self):
-        super().__init__()
-
-        self.conv1dblock_1 = Conv1dBlock(40, 64)
-        self.conv1dblock_2 = Conv1dBlock(64, 128, kernel_size=3, padding=2, dilation=2)
-
-        self.conv1dblock_3 = Conv1dBlock(128, 256, kernel_size=3, padding=4, dilation=4)
-        self.conv1dblock_4 = Conv1dBlock(256, 512, kernel_size=3, padding=8, dilation=8)
-
         self.conv1dblock_5 = Conv1dBlock(512, 1024, kernel_size=3, padding=8, dilation=8)
         self.LSTM = nn.LSTM(1024, 1024, batch_first=True, bidirectional=True)
 
